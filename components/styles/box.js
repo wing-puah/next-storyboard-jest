60d312e6c908f99c0a370f0398ec6dcfd87f049d
unknown <puahhuiying@gmail.com>
2019-03-20 01:03:40 +0800

    update box

--- components/styles/box.js
+++ components/styles/box.js
@@ -1,8 +1,13 @@
 import styled from 'styled-components';
+import PropTypes from 'prop-types';
 
 const Box = styled.div`
-  border: 1px solid ${props => props.theme.palette.primary};
+  border: 1px solid ${props => props.color || props.theme.palette.primary};
   padding: 20px;
 `;
+
+Box.propTypes = {
+  color: PropTypes.string,
+};
 
 export default Box;
